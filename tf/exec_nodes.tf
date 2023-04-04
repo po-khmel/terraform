bd4a50e9c1f0ace53acc3a08dbbf03c2e315a17b
po.khmel <khmelevskayapv@gmail.com>
2023-04-04 11:58:26 +0200

    fixed ip

--- tf/exec_nodes.tf
+++ tf/exec_nodes.tf
@@ -73,7 +73,7 @@ resource "openstack_compute_instance_v2" "exec-node" {
               vars:
                 condor_role: execute
                 condor_copy_template: false
-                condor_host: ${openstack_compute_instance_v2.central-manager.network.1.fixed_ip_v4}
+                condor_host: ${openstack_compute_instance_v2.central-manager.network.0.fixed_ip_v4}
                 condor_password: ${var.condor_pass}
           tasks:
             - name: Disable pulsar
